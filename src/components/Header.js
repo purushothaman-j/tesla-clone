76af903f2f257229d2d6c0d4528db4a03d5a790f
purushothaman-j <purushothaman.j92@gmail.com>
2021-09-25 17:59:01 +0530

    added some animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,73 @@
 import React from "react";
 
+import styled from "styled-components";
+import MenuIcon from "@material-ui/icons/Menu";
 function Header() {
-  return <div></div>;
+  return (
+    <Container>
+      <a>
+        <img src="/images/logo.svg" alt="tesla" />
+      </a>
+      <Menu>
+        <a>Model S</a>
+
+        <a>Model 3</a>
+
+        <a>Model X</a>
+
+        <a>Model Y</a>
+      </Menu>
+      <RightMenu>
+        <a>Shop</a>
+        <a>Tesla Account</a>
+        <CustomMenu />
+      </RightMenu>
+    </Container>
+  );
 }
 
 export default Header;
+
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+`;
+
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  flex: 1;
+  justify-content: center;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: no-wrap;
+  }
+  @media (max-width: 769px) {
+    display: none;
+  }
+`;
+
+const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: no-wrap;
+    margin-right: 10px;
+  }
+`;
+
+const CustomMenu = styled(MenuIcon)`
+  cursor: pointer;
+`;
